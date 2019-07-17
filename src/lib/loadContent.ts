486adab57cd969ec64f103201d827cc2c2448032
+v <ljw@live.jp>
2019-07-17 09:53:36 +0800

    fix: remove replaceJSX

--- src/lib/loadContent.ts
+++ src/lib/loadContent.ts
@@ -2,10 +2,6 @@ import markdown from './markdown';
 import replaceArchor from './replaceArchor';
 import buildWrapper from './buildWrapper';
 
-function replaceJSX(source: string): string {
-  return source.replace(/([{}])/g, "{'$1'}");
-}
-
 interface Opt {
   markdown: object;
   anchor: string[] | boolean;
@@ -17,7 +13,6 @@ interface Opt {
 export default function(source: string, options: Opt): string {
   source = markdown(source, options.markdown);
   source = replaceArchor(source, options.anchor);
-  source = replaceJSX(source);
   source = buildWrapper(source, options.wrapper, {
     className: options.className,
     style: options.style,
