5b8fe229af00aea2ac07d32435187fcae46f3b6e
chiaweilee <ljw@live.jp>
2021-09-10 23:14:55 +0800

    feat: anchor

--- src/render.ts
+++ src/render.ts
@@ -2,8 +2,16 @@ export default (rawHtml: string): string => `import React from 'react';
   export default function(props) {
     // scroll to anchor after page component loaded
       React.useEffect(() => {
-        if (props?.location?.hash) {
-          // AnchorLink.scrollToAnchor(decodeURIComponent(props.location.hash.slice(1)));
+        if (props.location && props.location.hash) {
+          const anchor = props.location.hash.slice(1);
+          // wait for dom update
+          window.requestAnimationFrame(() => {
+            const elm = document.getElementById(decodeURIComponent(anchor));
+            if (elm) {
+              // compatible in Edge
+              window.scrollTo(0, getElmScrollPosition(elm) - 100);
+            }
+          });
         }
       }, []);
     return (${rawHtml});
